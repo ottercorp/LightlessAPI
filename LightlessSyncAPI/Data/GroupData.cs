43be25cc45eff1966d82ff254110ade2cbd07278
CakeAndBanana <admin@cakeandbanana.nl>
2025-09-17 02:38:21 +0200

    Added get of group profile, removed group from model. redone group data.

--- LightlessSyncAPI/Data/GroupData.cs
+++ LightlessSyncAPI/Data/GroupData.cs
@@ -3,17 +3,11 @@
 namespace LightlessSync.API.Data;
 
 [MessagePackObject(keyAsPropertyName: true)]
-public record GroupData(string GID, string? Alias = null, DateTime? CreatedAt = null, string? Description = null, string? Tags = null, string? Avatar = null)
+public record GroupData(string GID, string? Alias = null, DateTime? CreatedAt = null)
 {
     [IgnoreMember]
     public string AliasOrGID => string.IsNullOrWhiteSpace(Alias) ? GID : Alias;
     [IgnoreMember]
     public DateTime? CreatedAt { get; set; } = CreatedAt;
-    [IgnoreMember]
-    public string? Description { get; set; } = Description;
-    [IgnoreMember]
-    public string? Tags { get; set; } = Tags;
-    [IgnoreMember]
-    public string? Avatar { get; set; } = Avatar;
 
 }
