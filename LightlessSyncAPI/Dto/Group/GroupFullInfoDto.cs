69055b0f323e6d35f55750fd1dc5659a8e36b085
CakeAndBanana <admin@cakeandbanana.nl>
2025-09-25 03:34:36 +0200

    Change in groupfullinfo

--- LightlessSyncAPI/Dto/Group/GroupFullInfoDto.cs
+++ LightlessSyncAPI/Dto/Group/GroupFullInfoDto.cs
@@ -6,10 +6,9 @@ namespace LightlessSync.API.Dto.Group;
 
 [MessagePackObject(keyAsPropertyName: true)]
 public record GroupFullInfoDto(GroupData Group, UserData Owner, GroupPermissions GroupPermissions,
-    GroupUserPreferredPermissions GroupUserPermissions, GroupPairUserInfo GroupUserInfo, DateTime? GroupUserJoinedAt,
+    GroupUserPreferredPermissions GroupUserPermissions, GroupPairUserInfo GroupUserInfo,
     Dictionary<string, GroupPairUserInfo> GroupPairUserInfos) : GroupInfoDto(Group, Owner, GroupPermissions)
 {
     public GroupUserPreferredPermissions GroupUserPermissions { get; set; } = GroupUserPermissions;
     public GroupPairUserInfo GroupUserInfo { get; set; } = GroupUserInfo;
-    public DateTime? GroupUserJoinedAt { get; set; } = GroupUserJoinedAt;
 }
