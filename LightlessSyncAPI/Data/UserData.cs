eb04433427d8b5144688004e436d833c6b63d39c
azyges <229218900+azyges@users.noreply.github.com>
2025-09-26 17:59:11 +0900

    include vanity and colors

--- LightlessSyncAPI/Data/UserData.cs
+++ LightlessSyncAPI/Data/UserData.cs
@@ -1,9 +1,16 @@
-﻿using MessagePack;
+using MessagePack;
 
 namespace LightlessSync.API.Data;
 
 [MessagePackObject(keyAsPropertyName: true)]
-public record UserData(string UID, string? Alias = null, bool IsAdmin = false, bool IsModerator = false)
+public record UserData(
+    string UID,
+    string? Alias = null,
+    bool IsAdmin = false,
+    bool IsModerator = false,
+    bool HasVanity = false,
+    string? TextColorHex = "",
+    string? TextGlowColorHex = "")
 {
     [IgnoreMember]
     public string AliasOrUID => string.IsNullOrWhiteSpace(Alias) ? UID : Alias;
