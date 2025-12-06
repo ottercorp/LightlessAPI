8ea337ab2de8724d941ac96597f414d2149932bc
cake <admin@cakeandbanana.nl>
2025-12-06 18:02:39 +0100

    Fixed some problems.

--- LightlessSyncAPI/SignalR/ILightlessHub.cs
+++ LightlessSyncAPI/SignalR/ILightlessHub.cs
@@ -68,7 +68,7 @@ public interface ILightlessHub
     Task<GroupProfileDto> GroupGetProfile(GroupDto dto);
     Task GroupSetProfile(GroupProfileDto dto);
     Task GroupSetUserInfo(GroupPairUserInfoDto groupPair);
-    Task<GroupPruneSettingsDto?> GroupGetPruneSettings(GroupDto dto);
+    Task<GroupPruneSettingsDto> GroupGetPruneSettings(GroupDto dto);
     Task GroupSetPruneSettings(GroupPruneSettingsDto dto);
     Task<List<GroupFullInfoDto>> GroupsGetAll();
     Task GroupUnbanUser(GroupPairDto groupPair);
