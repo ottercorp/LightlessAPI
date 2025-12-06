efb5a8072b99ba35419ee11225094a099ea304a5
cake <admin@cakeandbanana.nl>
2025-12-06 20:16:03 +0100

    Added MessagePackObject in dto.

--- LightlessSyncAPI/Dto/Group/GroupPruneSettingsDto.cs
+++ LightlessSyncAPI/Dto/Group/GroupPruneSettingsDto.cs
@@ -1,6 +1,7 @@
 ﻿using LightlessSync.API.Data;
+using MessagePack;
 
-namespace LightlessSync.API.Dto.Group
-{
-    public record GroupPruneSettingsDto(GroupData Group, bool AutoPruneEnabled, int AutoPruneDays);
-}
+namespace LightlessSync.API.Dto.Group;
+
+[MessagePackObject(keyAsPropertyName: true)]
+public record GroupPruneSettingsDto(GroupData Group, bool AutoPruneEnabled, int AutoPruneDays);
