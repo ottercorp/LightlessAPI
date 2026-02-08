f066e5dcaffd2d1a105648bfa2cdeb892ecfac56
cake <admin@cakeandbanana.nl>
2026-02-08 22:52:08 +0100

    Added blacklist functions on server side

---
+++ LightlessSyncAPI/Dto/User/UserBlacklistDto.cs
@@ -0,0 +1,6 @@
+﻿using LightlessSync.API.Data;
+
+namespace LightlessSync.API.Dto.User
+{
+    public record UserBlacklistDto(UserData blacklistedUser, DateTime blockedDate) : UserDto(blacklistedUser);
+}
