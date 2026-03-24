0b0d6d5d57f3d27241064c09fce3f5d75413e6cd
cake <cake@noreply.git.lightless-sync.org>
2026-03-24 01:00:45 +0000

    Added a getter for users in a group (#25)
    Co-authored-by: cake <admin@cakeandbanana.nl>
    Reviewed-on: https://git.lightless-sync.org/Lightless-Sync/LightlessAPI/pulls/25
    Co-authored-by: cake <cake@noreply.git.lightless-sync.org>
    Co-committed-by: cake <cake@noreply.git.lightless-sync.org>

---
+++ LightlessSyncAPI/Dto/Group/GroupUserDto.cs
@@ -0,0 +1,14 @@
+﻿using LightlessSync.API.Data;
+using LightlessSync.API.Data.Enum;
+
+
+namespace LightlessSync.API.Dto.Group
+{
+    public record GroupUserDto(
+        GroupData Group,
+        UserData User,
+        GroupPairUserInfo GroupUserInfo,
+        DateTime? JoinedOn,
+        bool FromFinder
+    );
+}
