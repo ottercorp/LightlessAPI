b45f34c805d295d6e4f395d4a5e926112a2a601b
Tsubasahane <wozaiha@gmail.com>
2026-05-08 10:17:43 +0800

    Update VenueData.cs

--- LightlessSyncAPI/Data/VenueData.cs
+++ LightlessSyncAPI/Data/VenueData.cs
@@ -8,7 +8,7 @@ public record VenueData(
     Guid Id,
     string Title,
     string? Description,
-    string UserID,
+    UserData user,
     DateTime UpdatedAt,
     DateTime StartAt,
     DateTime EndAt,
